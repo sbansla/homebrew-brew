7695e7a6fff5c3e0dbdcf25d5ac1eb31dc6fb52d
twilio-dx <team_interfaces+github@twilio.com>
2023-01-31 23:23:03 +0530

    Release 1.0.5-rc.13

--- Formula/twiliorc@1.0.5-rc.12.rb
+++ Formula/twiliorc@1.0.5-rc.12.rb
@@ -1,10 +1,10 @@
 require "language/node"
 
-class Twiliorc < Formula
+class twiliorcAT105Urc12 < Formula
   desc "unleash the power of Twilio from your command prompt"
   homepage "https://github.com/twilio/twilio-cli"
-  url "https://twilio-cli-prod.s3.amazonaws.com/channels/rc/twilio-v1.0.5-rc.13/twilio-v1.0.5-rc.13.tar.gz"
-  version "1.0.5-rc.13"
+  url "https://twilio-cli-prod.s3.amazonaws.com/channels/rc/twilio-v1.0.5-rc.12/twilio-v1.0.5-rc.12.tar.gz"
+  version "1.0.5-rc.12"
   sha256 "dummySha"
   depends_on "node"
 
